e5dc5a061b34f5a2ddea72d49d82894707e953b5
vibhaw raj <vibhawraj494@gmail.com>
2024-08-25 19:03:08 +0530

    ..

--- src/app/(pages)/_not-found/page.tsx
+++ src/app/(pages)/_not-found/page.tsx
@@ -7,7 +7,7 @@
 //       <h1 className="text-6xl font-bold text-gray-800">404</h1>
 //       <h2 className="text-3xl font-semibold text-gray-600 mt-4">Page Not Found</h2>
 //       <p className="text-xl text-gray-500 mt-4 mb-8">
-//       Oops! The page you&apos;re looking for doesn&apos;t exist.
+//         Oops! The page you&apos;re looking for doesn&apos;t exist.
 //       </p>
 //       <Link href="/" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300">
 //         Go Home
